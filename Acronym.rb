9c6e549170aac8f833674ce7a92fa5ffca27d67e
Rohit <rohitpratti@gmail.com>
2019-08-21 19:45:21 -0700

    LOL

--- Acronym.rb
+++ Acronym.rb
@@ -4,11 +4,12 @@ lines = ARGF.read.split("\n").join(" ")
 
 
 def acronym(input)
- input.split(" ").collect do |word|
+ arr = input.split(" ").collect do |word|
     if word[0] == word[0].upcase
         word[0]
     end
   end.compact.join
+  puts arr
 end
 
 acronym(lines)
